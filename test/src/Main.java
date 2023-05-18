71f49c0d148fd45b0d7327af00ef6d37563690c5
zhyunk <gimwlgus@gmail.com>
2023-05-19 00:08:27 +0900

    test
    어렵다;
    어려워;
    깃 main branch commit push test

--- test/src/Main.java
+++ test/src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("가나다라마바상ㅈㅊㅋㅌㅍㅎ");
     }
+
+    void test() {
+        System.out.println("asdjlaskdjlk");
+    }
 }
