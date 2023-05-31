0751ac5976068bc2e257af2decbecbb3089c8da2
zhyunk <gimwlgus@gmail.com>
2023-06-01 01:03:43 +0900

    test 1 update

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("가나다라마바상ㅈㅊㅋㅌㅍㅎ");
+        System.out.println("feature test 1 다녀갑니다");
     }
 
     void test() {
